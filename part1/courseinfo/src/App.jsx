57076f1a29b170d3dcbcd78a818bdd750104941f
Edward <github@edwardcs.com>
2023-03-12 21:30:17 +0200

    Solve: 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,6 +4,16 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
     <p>
       {props.part} {props.exercise}
     </p>
@@ -31,9 +41,10 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   );
