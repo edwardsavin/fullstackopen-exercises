2be8455798442f6ee089fd618301cf9ef7598b9b
Edward <github@edwardcs.com>
2023-03-18 03:13:05 +0200

    Solve: 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,44 +1,4 @@
-const Header = ({ course }) => {
-  return <h3>{course}</h3>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part.name} exercise={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Part = ({ part, exercise }) => {
-  return (
-    <p>
-      {part} {exercise}
-    </p>
-  );
-};
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((acc, part) => acc + part.exercises, 0);
-
-  return <b>total of {totalExercises} exercises</b>;
-};
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map((course) => (
-        <div key={course.id}>
-          <Header course={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
-        </div>
-      ))}
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
