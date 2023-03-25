e330a9bfda46570336e0f0efdab5e40cc275cc2c
Edward <github@edwardcs.com>
2023-03-25 19:54:21 +0200

    Solve: 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,10 @@
 const { request, response } = require("express");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const getMorgan = morgan("tiny");
